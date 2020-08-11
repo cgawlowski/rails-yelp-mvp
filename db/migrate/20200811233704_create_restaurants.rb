d2493f3c621303fb21b5b39fe0edd9522d61039c
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2020-08-11 19:46:43 -0400

    Added restaurant and review models

---
+++ db/migrate/20200811233704_create_restaurants.rb
@@ -0,0 +1,13 @@
+class CreateRestaurants < ActiveRecord::Migration[6.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.integer :phone_number
+      t.text :category
+      t.string :reviews
+
+      t.timestamps
+    end
+  end
+end
